2ef49b15c51506e2af64a2d18727a33b29e9ff20
simhachalam402 <chalam.pydana@gmail.com>
2020-12-07 16:22:14 +0530

    update with validation for the test case

--- src/test/java/com/example/assignment/AssignmentRepositoryTests.java
+++ src/test/java/com/example/assignment/AssignmentRepositoryTests.java
@@ -1,6 +1,7 @@
 package com.example.assignment;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertTrue;
 
 import java.util.List;
 import java.util.Optional;
@@ -40,15 +41,16 @@ public class AssignmentRepositoryTests {
 		LOGGER.info("whenFindAll case got triggered >> ");
 		Person p1 = new Person("Test1", "T1");
 		Person p2 = new Person("Test2", "T2");
-		personRepository.saveAndFlush(p1);
-		personRepository.saveAndFlush(p2);
+		Person p1_save = personRepository.saveAndFlush(p1);
+		Person p2_save = personRepository.saveAndFlush(p2);
 		
 		List<Person> personsList = personRepository.findAll();
 		
-		assertThat(personsList.size()).isEqualTo(2);
-		assertThat(personsList.get(0)).isEqualTo(p1);
-		assertThat(personsList.get(1)).isEqualTo(p2);
-		LOGGER.info("whenFindAll case got finished >> ");
+		assertThat(personsList.size()).isNotEqualTo(0);
+		assertTrue(personsList.contains(p1_save));
+		assertThat(personsList.get(0)).isEqualTo(p1_save);
+		assertThat(personsList.get(1)).isEqualTo(p2_save);
+		LOGGER.info("whenFindAll case got finished >> {}", personsList);
 	}
 	
 	@Test
@@ -57,13 +59,13 @@ public class AssignmentRepositoryTests {
 		LOGGER.info("whenFindAllById case got triggered >> ");
 		Person p3 = new Person("Test3", "T3");
 		Person p4 = new Person("Test4", "T4");
-		Person p3_book = personRepository.saveAndFlush(p3);
+		Person p3_save = personRepository.saveAndFlush(p3);
 		personRepository.saveAndFlush(p4);
 		
-		Optional<Person> testPerson = personRepository.findById(p3_book.getId());
-		
+		Optional<Person> testPerson = personRepository.findById(p3_save.getId());
+		assertThat(testPerson.get().getId()).isEqualTo(p3.getId());
 		assertThat(testPerson.get().getFirstName()).isEqualTo(p3.getFirstName());
-		LOGGER.info("whenFindAllById case got finished >> ");
+		LOGGER.info("whenFindAllById case got finished >> {}", testPerson.get());
 	}
 	
 	@Test
@@ -72,19 +74,21 @@ public class AssignmentRepositoryTests {
 		LOGGER.info("whenDeleteById case got triggered >> ");
 		Person p5 = new Person("Test5", "T5");
 		Person p6 = new Person("Test6", "T6");
-		Person p5_book = personRepository.saveAndFlush(p5);
+		Person p5_save = personRepository.saveAndFlush(p5);
 		personRepository.saveAndFlush(p6);
 		
-		Optional<Person> testPerson = personRepository.findById(p5_book.getId());
+		Optional<Person> testPerson = personRepository.findById(p5_save.getId());
 		if(testPerson.isPresent()) {
+			LOGGER.info("Fetched person object for deletion process is {}", p5_save);
 			personRepository.delete(testPerson.get());
-			Person pp = personRepository.findById(p5_book.getId()).orElse(null);
+			Person pp = personRepository.findById(p5_save.getId()).orElse(null);
 			if(pp==null) {
 				assertThat(true);
 			} else {
 				assertThat(false);
 			}
 		} else {
+			LOGGER.info("Looking for person id {} is not available.", p5_save.getId());
 			assertThat(false);
 		}
 		LOGGER.info("whenDeleteById case got finished >> ");
@@ -96,22 +100,29 @@ public class AssignmentRepositoryTests {
 		LOGGER.info("whenUpdateById case got triggered >> ");
 		Person p7 = new Person("Test7", "T7");
 		Person p8 = new Person("Test8", "T8");
-		Person p7_book = personRepository.saveAndFlush(p7);
+		Person p7_save = personRepository.saveAndFlush(p7);
 		personRepository.saveAndFlush(p8);
 		
-		Optional<Person> testPerson = personRepository.findById(p7_book.getId());
+		Optional<Person> testPerson = personRepository.findById(p7_save.getId());
 		if(testPerson.isPresent()) {
+			LOGGER.info("Fetched person object for updation process is available in db. Person Id: ", p7_save.getId());
 			Person updateObj = testPerson.get();
 			updateObj.setFirstName("Test7-1");
 			updateObj.setLastName("T7-1");
-			Person p9_book = personRepository.saveAndFlush(updateObj);
-			testPerson = personRepository.findById(p9_book.getId());
+			Person p9_save = personRepository.saveAndFlush(updateObj);
+			LOGGER.info("Fetched person object is updated successfully. And person object is {}", p9_save);
+			assertThat(p9_save!=null);
+			
+			testPerson = personRepository.findById(p9_save.getId());
 			if(testPerson.isPresent()) {
+				LOGGER.info("Fetch updated person object is availeble in db. {}", testPerson.get());
 				assertThat(testPerson.get().getFirstName()).isEqualTo("Test7-1");
 			} else {
+				LOGGER.info("Fetch updated person object is not available in db. {}", testPerson.get());
 				assertThat(false);
 			}
 		} else {
+			LOGGER.info("Couldn't find the person object which needs to update. PersonId: ",p7_save.getId());
 			assertThat(false);
 		}
 		LOGGER.info("whenUpdateById case got finished >> ");
